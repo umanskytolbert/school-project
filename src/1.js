0b521bc7423917bc87ab076d58fbef980793bf72
umanskytolbert <umanskytolbert@gmail.com>
2025-03-05 19:40:18 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,7 @@
+// Function to get a random number between 1 and 10
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+// Example usage
+console.log(getRandomNumber());
