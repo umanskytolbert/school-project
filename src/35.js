1996842796f44c88ebce8e820b12abfad162e8e2
umanskytolbert <umanskytolbert@gmail.com>
2025-04-11 12:22:58 +0000

    Add 35.js

---
+++ src/35.js
@@ -0,0 +1,11 @@
+const { fetch } = require('node-fetch');
+
+// Fetch data from API
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
